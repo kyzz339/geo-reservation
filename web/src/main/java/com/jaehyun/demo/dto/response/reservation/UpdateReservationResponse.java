ab594abde89a27dded6ac73bf027cd96b11447fc
JAEHYUN KWON <kyzz339@naver.com>
2026-02-12 22:31:12 +0900

    정상 테스트 케이스 생성 mockito

--- web/src/main/java/com/jaehyun/demo/dto/response/reservation/UpdateReservationResponse.java
+++ web/src/main/java/com/jaehyun/demo/dto/response/reservation/UpdateReservationResponse.java
@@ -1,6 +1,7 @@
 package com.jaehyun.demo.dto.response.reservation;
 
 import com.jaehyun.demo.core.entity.Reservation;
+import com.jaehyun.demo.core.enums.ReservationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +14,12 @@ import lombok.NoArgsConstructor;
 public class UpdateReservationResponse {
 
     private Long id;
+    private ReservationStatus status;
 
     public static UpdateReservationResponse from(Reservation reservation){
         return UpdateReservationResponse.builder()
                 .id(reservation.getId())
+                .status(reservation.getStatus())
                 .build();
     }
 
--- web/src/test/java/com/jaehyun/demo/service/unitTest/ReservationServcieTest_mockito.java
+++ web/src/test/java/com/jaehyun/demo/service/unitTest/ReservationServcieTest_mockito.java
@@ -8,12 +8,15 @@ import com.jaehyun.demo.core.dao.UserDao;
 import com.jaehyun.demo.core.entity.Reservation;
 import com.jaehyun.demo.core.entity.Store;
 import com.jaehyun.demo.core.entity.User;
+import com.jaehyun.demo.core.enums.ReservationStatus;
 import com.jaehyun.demo.core.enums.Role;
 import com.jaehyun.demo.dto.request.reservation.CreateReservationRequest;
-import com.jaehyun.demo.dto.response.reservation.CreateReservationResponse;
+import com.jaehyun.demo.dto.request.reservation.ReservationRequest;
+import com.jaehyun.demo.dto.request.reservation.UpdateReservationRequest;
+import com.jaehyun.demo.dto.response.reservation.ReservationResponse;
+import com.jaehyun.demo.dto.response.reservation.UpdateReservationResponse;
 import com.jaehyun.demo.service.ReservationService;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,11 +26,11 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -161,8 +164,253 @@ public class ReservationServcieTest_mockito {
         assertEquals(exception.getErrorCode() , ErrorCode.CAPACITY_EXCEEDED);
     }
 
-    //본인 가게 리스트 확인
+    //본인 가게 리스트 확인 - 가게주인
+    @Test
+    @DisplayName("본인 가게 리스트 확인")
+    void displayMyStore(){
+
+        String email = "test@test.com";
+
+        User user = User.builder()
+                .id(1L)
+                .email(email)
+                .name("name")
+                .type(Role.OWNER)
+                .build();
+
+        UserDetails userDetails = mock(UserDetails.class);
+
+        when(userDetails.getUsername()).thenReturn(email);
+
+        Store existStore = Store.builder()
+                .id(1L)
+                .name("테스트 가게")
+                .description("테스트 가게입니다")
+                .location(geomereyFactory.createPoint(new Coordinate(17.0 , 18.0)))
+                .address("주소")
+                .maxCapacity(10)
+                .active(true)
+                .createdAt(LocalDateTime.now())
+                .owner(user)
+                .build();
+
+        List<Reservation> reservations = Arrays.asList(
+                Reservation.builder()
+                        .id(1L)
+                        .user(user)
+                        .store(existStore)
+                        .visitorCount(5)
+                        .reservedAt(LocalDateTime.now())
+                        .finishedAt(LocalDateTime.now().plusHours(1))
+                        .status(ReservationStatus.PENDING)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build(),
+
+                Reservation.builder()
+                        .id(2L)
+                        .user(user)
+                        .store(existStore)
+                        .visitorCount(5)
+                        .reservedAt(LocalDateTime.now().plusDays(1))
+                        .finishedAt(LocalDateTime.now().plusDays(1).plusHours(1))
+                        .status(ReservationStatus.PENDING)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build()
+        );
+
+        ReservationRequest request = ReservationRequest.builder()
+                        .storeId(1L)
+                        .build();
+
+
+        when(storeDao.getStore(1L)).thenReturn(Optional.of(existStore));
+        when(reservationDao.viewReservations(1L)).thenReturn(reservations);
+
+        List<ReservationResponse> response =  reservationService.viewStoreReservation(request , userDetails);
+
+        verify(storeDao , times(1)).getStore(1L);
+        verify(reservationDao , times(1)).viewReservations(1L);
+
+        assertNotNull(reservations);
+        assertEquals(1L , response.get(0).getId());
+        assertEquals(2L , response.get(1).getId());
+    }
     //손님 예약 확인 list
+    @Test
+    @DisplayName("손님-예약확인 List")
+    void viewMyReservation(){
+
+        String email = "test@test.com";
+
+        User existCustomer = User.builder()
+                .id(1L)
+                .email(email)
+                .name("name")
+                .type(Role.USER)
+                .build();
+
+        UserDetails userDetails = mock(UserDetails.class);
+
+        Store existStore = Store.builder()
+                .id(1L)
+                .name("테스트 가게")
+                .description("테스트 가게입니다")
+                .location(geomereyFactory.createPoint(new Coordinate(17.0 , 18.0)))
+                .address("주소")
+                .maxCapacity(10)
+                .active(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        List<Reservation> reservations = Arrays.asList(
+                Reservation.builder()
+                        .id(1L)
+                        .user(existCustomer)
+                        .store(existStore)
+                        .visitorCount(5)
+                        .reservedAt(LocalDateTime.now())
+                        .finishedAt(LocalDateTime.now().plusHours(1))
+                        .status(ReservationStatus.PENDING)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build(),
+
+                Reservation.builder()
+                        .id(2L)
+                        .user(existCustomer)
+                        .store(existStore)
+                        .visitorCount(5)
+                        .reservedAt(LocalDateTime.now().plusDays(1))
+                        .finishedAt(LocalDateTime.now().plusDays(1).plusHours(1))
+                        .status(ReservationStatus.PENDING)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build()
+        );
+
+        when(userDetails.getUsername()).thenReturn(email);
+        when(reservationDao.viewMyReservations(email)).thenReturn(reservations);
+
+        List<ReservationResponse> response = reservationService.viewMyReservation(userDetails);
+
+        verify(reservationDao , times(1)).viewMyReservations(email);
+
+        assertEquals(1L , response.get(0).getId());
+        assertEquals(2L , response.get(1).getId());
+
+    }
     //예약 취소
+    @Test
+    @DisplayName("예약 취소")
+    void cancelReservation(){
+        String email = "test@test.com";
+
+        User existCustomer = User.builder()
+                .id(1L)
+                .email(email)
+                .name("name")
+                .type(Role.USER)
+                .build();
+
+        UserDetails userDetails = mock(UserDetails.class);
+
+        Store existStore = Store.builder()
+                .id(1L)
+                .name("테스트 가게")
+                .description("테스트 가게입니다")
+                .location(geomereyFactory.createPoint(new Coordinate(17.0 , 18.0)))
+                .address("주소")
+                .maxCapacity(10)
+                .active(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Reservation exsistReservation =  Reservation.builder()
+                .id(1L)
+                .user(existCustomer)
+                .store(existStore)
+                .visitorCount(5)
+                .reservedAt(LocalDateTime.now())
+                .finishedAt(LocalDateTime.now().plusHours(1))
+                .status(ReservationStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        when(reservationDao.viewReservation(1L)).thenReturn(Optional.of(exsistReservation));
+        when(userDetails.getUsername()).thenReturn(email);
+
+        UpdateReservationRequest request = UpdateReservationRequest.builder()
+                .id(1L)
+                .build();
+
+
+        UpdateReservationResponse response = reservationService.cancelReservation(request , userDetails);
+
+        verify(reservationDao , times(1)).viewReservation(1L);
+
+        assertEquals(response.getId() , request.getId());
+        assertEquals(response.getStatus() , ReservationStatus.CANCELED);
+    }
     //예약 변경
+    @Test
+    @DisplayName("예약 변경")
+    void changeReservation(){
+
+        String email = "test@test.com";
+
+        User existCustomer = User.builder()
+                .id(1L)
+                .email(email)
+                .name("name")
+                .type(Role.USER)
+                .build();
+
+        Store existStore = Store.builder()
+                .id(1L)
+                .name("테스트 가게")
+                .description("테스트 가게입니다")
+                .location(geomereyFactory.createPoint(new Coordinate(17.0 , 18.0)))
+                .address("주소")
+                .maxCapacity(10)
+                .active(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Reservation exsistReservation =  Reservation.builder()
+                .id(1L)
+                .user(existCustomer)
+                .store(existStore)
+                .visitorCount(5)
+                .reservedAt(LocalDateTime.now())
+                .finishedAt(LocalDateTime.now().plusHours(1))
+                .status(ReservationStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        UserDetails userDetails = mock(UserDetails.class);
+
+        when(reservationDao.viewReservation(1L)).thenReturn(Optional.of(exsistReservation));
+        when(userDetails.getUsername()).thenReturn(email);
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now().plusHours(1);
+        when(reservationDao.getSumVisitorCountExcludeMine(1L , start , end , email)).thenReturn(5);
+
+        UpdateReservationRequest request = UpdateReservationRequest.builder()
+                .id(1L)
+                .reservedAt(start)
+                .finishedAt(end)
+                .visitorCount(3)
+                .build();
+
+        UpdateReservationResponse response = reservationService.changeReservation(request , userDetails);
+
+        verify(reservationDao , times(1)).viewReservation(1L);
+
+        assertEquals(response.getId() , request.getId());
+        assertEquals(response.getStatus() , ReservationStatus.PENDING);
+    }
 }
